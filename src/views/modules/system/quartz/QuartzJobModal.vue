4139e9abc88a5a95877ac330bb5be2c9901d1803
xxm <xxm1995@outlook.com>
2021-10-20 20:57:28 +0800

    feat 定时任务页面搭建

---
+++ src/views/modules/system/quartz/QuartzJobModal.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'QuartzJobModal',
+  data(){
+    return {
+      queryParam:{}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

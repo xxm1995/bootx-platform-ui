6f38f7db552a91681ec92ef08bda30c49f14a219
xxm <xxm1995@outlook.com>
2022-01-06 21:52:00 +0800

    feat 开始优化设置和个人中心界面

--- src/views/account/settings/BasicSetting.vue
+++ src/views/account/settings/BasicSetting.vue
@@ -1,33 +1,35 @@
 <template>
   <div class="account-settings-info-view">
-    <a-row :gutter="16" type="flex" justify="center">
-      <a-col :order="isMobile ? 2 : 1" :md="24" :lg="16">
-        <a-form-model layout="vertical">
-          <a-form-item
-            label="昵称"
+    <a-row :order="isMobile ? 2 : 1" :gutter="16" type="flex" justify="start">
+      <a-col :md="12" :lg="12" >
+        <a-form-model
+          :labelCol="labelCol"
+          :wrapperCol="wrapperCol"
+        >
+          <a-form-model-item
+            label="姓名"
+            prop="name"
           >
-            <a-input placeholder="请输入您的昵称!" />
-          </a-form-item>
-          <a-form-item
-            label="个人简介"
+            <a-input />
+          </a-form-model-item>
+          <a-form-model-item
+            label="性别"
+            prop="sex"
           >
-            <a-textarea rows="4" placeholder="请输入个人简介!'"/>
-          </a-form-item>
-
-          <a-form-item
-            label="邮箱"
-            :required="false"
+            <a-input />
+          </a-form-model-item>
+          <a-form-model-item
+            label="生日"
+            prop="birthday"
           >
-            <a-input placeholder="example@ant.design"/>
-          </a-form-item>
-
-          <a-form-item>
+            <a-input />
+          </a-form-model-item>
+          <a-form-model-item>
             <a-button type="primary">更新基本信息</a-button>
-          </a-form-item>
+          </a-form-model-item>
         </a-form-model>
-
       </a-col>
-      <a-col :order="1" :md="24" :lg="8" :style="{ minHeight: '180px' }">
+      <a-col :order="1" :md="12" :lg="12" :style="{ minHeight: '180px' }">
         <div class="ant-upload-preview" @click="$refs.modal.edit(1)" >
           <a-icon type="cloud-upload-o" class="upload-icon"/>
           <div class="mask">
@@ -57,6 +59,12 @@ export default {
     return {
       // cropper
       preview: {},
+      labelCol: {
+        sm: { span: 4 }
+      },
+      wrapperCol: {
+        sm: { span: 15 }
+      },
       option: {
         img: '/avatar2.jpg',
         info: true,
@@ -84,57 +92,57 @@ export default {
 
 <style lang="less" scoped>
 
-  .avatar-upload-wrapper {
-    height: 200px;
-    width: 100%;
+.avatar-upload-wrapper {
+  height: 200px;
+  width: 100%;
+}
+
+.ant-upload-preview {
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 180px;
+  border-radius: 50%;
+  box-shadow: 0 0 4px #ccc;
+
+  .upload-icon {
+    position: absolute;
+    top: 0;
+    right: 10px;
+    font-size: 1.4rem;
+    padding: 0.5rem;
+    background: rgba(222, 221, 221, 0.7);
+    border-radius: 50%;
+    border: 1px solid rgba(0, 0, 0, 0.2);
+  }
+  .mask {
+    opacity: 0;
+    position: absolute;
+    background: rgba(0,0,0,0.4);
+    cursor: pointer;
+    transition: opacity 0.4s;
+
+    &:hover {
+      opacity: 1;
+    }
+
+    i {
+      font-size: 2rem;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin-left: -1rem;
+      margin-top: -1rem;
+      color: #d6d6d6;
+    }
   }
 
-  .ant-upload-preview {
-    position: relative;
-    margin: 0 auto;
+  img, .mask {
     width: 100%;
     max-width: 180px;
+    height: 100%;
     border-radius: 50%;
-    box-shadow: 0 0 4px #ccc;
-
-    .upload-icon {
-      position: absolute;
-      top: 0;
-      right: 10px;
-      font-size: 1.4rem;
-      padding: 0.5rem;
-      background: rgba(222, 221, 221, 0.7);
-      border-radius: 50%;
-      border: 1px solid rgba(0, 0, 0, 0.2);
-    }
-    .mask {
-      opacity: 0;
-      position: absolute;
-      background: rgba(0,0,0,0.4);
-      cursor: pointer;
-      transition: opacity 0.4s;
-
-      &:hover {
-        opacity: 1;
-      }
-
-      i {
-        font-size: 2rem;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-left: -1rem;
-        margin-top: -1rem;
-        color: #d6d6d6;
-      }
-    }
-
-    img, .mask {
-      width: 100%;
-      max-width: 180px;
-      height: 100%;
-      border-radius: 50%;
-      overflow: hidden;
-    }
+    overflow: hidden;
   }
+}
 </style>
--- src/views/account/settings/Security.vue
+++ src/views/account/settings/Security.vue
@@ -29,7 +29,6 @@ computed: {
         { title: this.$t('account.settings.security.phone'), description: this.$t('account.settings.security.phone-description'), value: '138****8293', actions: { title: this.$t('account.settings.security.modify'), callback: () => { this.$message.success('This is a message of success') } } },
         { title: this.$t('account.settings.security.question'), description: this.$t('account.settings.security.question-description'), value: '', actions: { title: this.$t('account.settings.security.set'), callback: () => { this.$message.error('This is a message of error') } } },
         { title: this.$t('account.settings.security.email'), description: this.$t('account.settings.security.email-description'), value: 'ant***sign.com', actions: { title: this.$t('account.settings.security.modify'), callback: () => { this.$message.warning('This is message of warning') } } },
-        { title: this.$t('account.settings.security.mfa'), description: this.$t('account.settings.security.mfa-description'), value: '', actions: { title: this.$t('account.settings.security.bind'), callback: () => { this.$message.info('This is a normal message') } } }
       ]
     }
   }

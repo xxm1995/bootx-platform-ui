d5940450929e11c7724684432d8e22f81bb84cde
xxm <xxm1995@outlook.com>
2022-09-03 19:38:20 +0800

    feat 流程图任务节点信息展示优化

--- src/views/modules/office/applyshow/ApplyFormShow.vue
+++ src/views/modules/office/applyshow/ApplyFormShow.vue
@@ -37,14 +37,47 @@
         </a-card>
       </a-tab-pane>
       <a-tab-pane key="history" tab="历史信息" force-render>
-        <a-timeline>
-          <a-timeline-item v-for="o in taskList" :key="o.id">
-            <p>开始时间: {{ o.startTime }}</p>
-            <p>环节: {{ o.taskName }}</p>
-            <p>处理人: {{ o.userName }}</p>
-            <p>结束时间: {{ o.endTime }}</p>
-          </a-timeline-item>
-        </a-timeline>
+        <a-row :gutter="64">
+          <a-col :span="8">
+            <a-card title="任务历史" :bordered="false">
+              <a-timeline>
+                <a-timeline-item v-for="o in taskList" :key="o.id">
+                  <p>开始时间: {{ o.startTime }}</p>
+                  <p>环节: {{ o.taskName }}</p>
+                  <p>处理人: {{ o.userName }}</p>
+                  <p>结束时间: {{ o.endTime }}</p>
+                </a-timeline-item>
+              </a-timeline>
+            </a-card>
+          </a-col>
+          <a-col :span="16">
+            <a-card title="流程信息" :bordered="false">
+              <a-descriptions :column="{md: 1}" bordered>
+                <a-descriptions-item label="流程实例名称">
+                  {{ instance.instanceName }}
+                </a-descriptions-item>
+                <a-descriptions-item label="流程定义的id">
+                  {{ instance.defId }}
+                </a-descriptions-item>
+                <a-descriptions-item label="流程定义的名称">
+                  {{ instance.defName }}
+                </a-descriptions-item>
+                <a-descriptions-item label="流程定义的名称">
+                  {{ instance.defName }}
+                </a-descriptions-item>
+                <a-descriptions-item label="发起人名称">
+                  {{ instance.startUserName }}
+                </a-descriptions-item>
+                <a-descriptions-item label="开始时间">
+                  {{ instance.startTime }}
+                </a-descriptions-item>
+                <a-descriptions-item label="结束时间">
+                  {{ instance.endTime }}
+                </a-descriptions-item>
+              </a-descriptions>
+            </a-card>
+          </a-col>
+        </a-row>
       </a-tab-pane>
       <a-tab-pane key="flowChart" tab="流程图" force-render>
         <process-viewer
